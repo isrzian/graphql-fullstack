aa5e191c2d65a3f6b3398c2e7704a5ed309fe0fe
IlyaRz <IlyaRz@WS101>
2021-09-08 12:54:48 +0700

    backend the end

--- server/index.js
+++ server/index.js
@@ -3,13 +3,40 @@ const {graphqlHTTP} = require('express-graphql')
 const cors = require('cors')
 const schema = require('./schema')
 
+const users = [
+	{id: 1, username: 'Ilya', age: 24},
+	{id: 2, username: 'Zhenya', age: 30},
+]
+
 const app = express()
 
 app.use(cors())
 
+const createUser = (input) => {
+	const id = Date.now()
+	return {
+		id, ...input
+	}
+}
+
+const root = {
+	getAllUsers: () => {
+		return users
+	},
+	getUser: ({id}) => {
+		return users.find(users => users.id === id)
+	},
+	createUser: ({input}) => {
+		const user = createUser(input)
+		users.push(user)
+		return user
+	}
+}
+
 app.use('/graphql', graphqlHTTP({
 	graphiql: true,
-	schema
+	schema,
+	rootValue: root
 }))
 
 const PORT = process.env.PORT || 5000
